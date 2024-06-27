e19f329e80d32e62553d9157ecbf863b9952c10d
Bruno Guide <bruno.fguide@gmail.com>
2024-06-27 10:08:48 -0300

    fixing js script - thx to chat gpt

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-    const headings = document.querySelectorAll('h2');
-    console.log(headings)
-    headings.forEach(function(heading) {
-        heading.addEventListener('click', function() {
-            let nextElement = heading.nextElementSibling;
-            if (nextElement && nextElement.tagName.toLowerCase() === 'p') {
-                if (nextElement.style.display === 'none') {
-                    nextElement.style.display = 'block';
-                } else {
-                    nextElement.style.display = 'none';
-                }
-            }
-        });
+const toggleVisibility = function(divID) {
+    if (divID.style.display === 'none' || divID.style.display === '') {
+        divID.style.display = 'block';
+    } else {
+        divID.style.display = 'none';
+    }
+}
+    
+const nextElement = function (elem) {
+    do {
+        elem = elem.nextSibling;
+    } while (elem && elem.nodeType !== 1);
+    return elem;        
+}
+
+document.querySelectorAll('h2').forEach((heading) => {
+    heading.addEventListener('click', function() {
+        const divToToggle = nextElement(heading);
+        if (divToToggle && divToToggle.tagName.toLowerCase() === 'div') {
+            toggleVisibility(divToToggle);
+        }
     });
+});
